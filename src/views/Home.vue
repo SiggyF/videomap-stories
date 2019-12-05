cdfd3c3cdb72ad5e2d89f4deb89f34267b642326
Fedor Baart <fedor.baart@deltares.nl>
2019-12-05 15:27:24 +0100

    links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,6 @@
 <template>
-  <div><router-link to="/stories/glacier">glacier</router-link></div>
+  <div>
+    <router-link to="/stories/glacier">glacier</router-link>
+    <router-link to="/stories/merowe">merowe</router-link>
+  </div>
 </template>
