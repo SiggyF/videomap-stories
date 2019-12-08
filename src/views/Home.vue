c626e721a7d09ee4fb8ccd16e703cc90018c13d2
Fedor Baart <fedor.baart@deltares.nl>
2019-12-08 09:46:49 -0800

    stories layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,56 @@
 <template>
-  <div>
-    <router-link to="/stories/glacier">glacier</router-link>
-    <router-link to="/stories/merowe">merowe</router-link>
-  </div>
+<div>
+  <v-container
+    class="fill-height"
+    fluid
+    >
+    <v-col
+      cols="12">
+      <v-row
+        align="center"
+        justify="center"
+        >
+        <v-card
+          v-for="(story, index) in stories"
+          :key="index"
+          class="ma-3 pa-2"
+          outlined
+          tile
+
+          >
+          <v-img
+            class="white--text align-end"
+            :src="story.image"
+            height="200px"
+            width="400px"
+            >
+            <v-card-title v-text="story.title">
+            </v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn icon :to="'/stories/' + story.id">
+              <v-icon>mdi-presentation-play</v-icon>
+            </v-btn>
+
+          </v-card-actions>
+        </v-card>
+
+
+      </v-row>
+    </v-col>
+  </v-container>
+</div>
 </template>
+<script>
+import stories from '@/assets/stories.json'
+export default {
+  data () {
+    return  {
+      stories
+    }
+  }
+
+}
+</script>
