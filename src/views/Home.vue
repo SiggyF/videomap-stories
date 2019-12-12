7d9517e6e71e06112000aad768214d27957cf192
Fedor Baart <fedor.baart@deltares.nl>
2019-12-12 11:31:23 -0800

    just maps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,96 +1,92 @@
 <template>
 <div>
-  <v-app>
-    <v-content>
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-        clipped
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+    clipped
+    >
+    <v-list dense>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-settings</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Settings</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar
+    app
+    clipped-left
+    >
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-toolbar-title>Video stories</v-toolbar-title>
+  </v-app-bar>
+  <v-container
+    class="fill-height"
+    fluid
+    >
+    <v-col
+      cols="12">
+      <v-row
+        align="center"
+        justify="center"
         >
-        <v-list dense>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-view-dashboard</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Dashboard</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-settings</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Settings</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-app-bar
-        app
-        clipped-left
-        >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <v-toolbar-title>Video stories</v-toolbar-title>
-      </v-app-bar>
-      <v-container
-        class="fill-height"
-        fluid
-        >
-        <v-col
-          cols="12">
-          <v-row
-            align="center"
-            justify="center"
+        <v-card
+
+          v-for="(story, index) in stories"
+          :key="index"
+          class="ma-3 story"
+          outlined
+          tile
+
+          >
+          <v-img
+            class="white--text align-end"
+            :src="story.image"
+            height="200px"
+            width="400px"
             >
-            <v-card
+          </v-img>
 
-              v-for="(story, index) in stories"
-              :key="index"
-              class="ma-3 story"
-              outlined
-              tile
+          <v-card-title v-text="story.title">
+          </v-card-title>
+          <v-card-subtitle>
+            {{ story.subtitle }}
+          </v-card-subtitle>
+          <v-card-text>
 
-              >
-              <v-img
-                class="white--text align-end"
-                :src="story.image"
-                height="200px"
-                width="400px"
-                >
-              </v-img>
+          </v-card-text>
 
-              <v-card-title v-text="story.title">
-              </v-card-title>
-              <v-card-subtitle>
-                {{ story.subtitle }}
-              </v-card-subtitle>
-              <v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
 
-              </v-card-text>
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+            <v-btn icon :to="{name: 'map', params: {id: story.id}}">
+              <v-icon>mdi-presentation-play</v-icon>
+            </v-btn>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                  <v-icon>mdi-share-variant</v-icon>
-                </v-btn>
-                <v-btn icon :to="{name: 'map', params: {id: story.id}}">
-                  <v-icon>mdi-presentation-play</v-icon>
-                </v-btn>
-
-              </v-card-actions>
-            </v-card>
+          </v-card-actions>
+        </v-card>
 
 
-          </v-row>
-        </v-col>
-      </v-container>
-    </v-content>
-  </v-app>
+      </v-row>
+    </v-col>
+  </v-container>
 </div>
 </template>
 <script>
@@ -110,7 +106,7 @@ export default {
   max-width: 400px;
 }
 .v-card.story .v-image  {
-max-height: 200px;
-height:  200px;
+  max-height: 200px;
+  height:  200px;
 }
 </style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
-  <v-mapbox :access-token="accessToken"></v-mapbox>
-</div>
+<v-container fluid class="fill-height">
+  <v-mapbox :access-token="accessToken" :map-style="mapStyle" ref="mapbox"></v-mapbox>
+</v-container>
 
 </template>
 <script>
 import Vue from 'vue'
+import _ from 'lodash'
 import Vue2MapboxGL from 'vue2mapbox-gl';
+
+import stories from '@/assets/stories.json'
 
 
 // You might want to import the relevant css, for example:
@@ -19,11 +22,51 @@ Vue.use(Vue2MapboxGL);
 
 export default {
   data () {
+    let story = _.find(stories, ['id', this.$route.params.id])
+    console.log('story', story)
     let token = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
     return {
-      accessToken: token
+      accessToken: token,
+      story: story,
+      mapStyle: story.style
+
+    }
+  },
+  mounted () {
+    // TODO: this should be in object
+    this.map.on('load', this.addLayers)
+
+  },
+  computed: {
+    map () {
+      let map = this.$refs.mapbox.map
+      return map
+    }
+
+  },
+  methods: {
+    addLayers () {
+      this.map.flyTo(
+        this.story.location
+      )
+      this.story.layers.forEach(
+        layer => {
+          console.log('adding layer', layer)
+          this.map.addLayer(layer)
+          // assume source has same name as layer
+          let source = this.map.getSource(layer.id)
+          if (source.player) {
+            source.player.play()
+          }
+        }
+      )
     }
   }
-
 }
 </script>
+<style>
+.mapboxgl-map {
+  height: 100%;
+  width: 100%;
+}
+</style>
